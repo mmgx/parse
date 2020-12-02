66b5a49b2bc35fdf7d40db7b8b34757484a9e1b3
Mihail M <r@gothundead.ru>
2020-12-02 20:57:27 +0300

    добавлены модель, миграция и контроллер для Марки

---
+++ app/Http/Controllers/MarkaController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Base\BaseController;
+use Illuminate\Http\Request;
+
+class MarkaController extends BaseController
+{
+    //
+}
---
+++ database/migrations/2020_12_02_175010_create_markas_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMarkasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('markas', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('marka_id');
+            $table->bigInteger('subcategory_id')->nullable();
+            $table->string('name');
+            $table->string('title');
+            $table->string('price')->nullable();
+            $table->text('description');
+            $table->string('image')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('markas');
+    }
+}
