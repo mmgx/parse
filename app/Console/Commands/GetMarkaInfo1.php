c0c673d724ea7de60c13a892a199ce71aa253e53
Mihail M <r@gothundead.ru>
2020-12-05 20:13:40 +0300

    добавлены доп.команды для разбивки загрузки марок

---
+++ app/Console/Commands/GetMarkaInfo1.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Marka;
+use App\Models\Subcategory;
+use App\Service\ParseService;
+use Illuminate\Console\Command;
+
+class GetMarkaInfo1 extends Command
+{
+    protected $parserService;
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'get:markas1';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct(ParseService $parserService)
+    {
+        parent::__construct();
+        $this->parserService = $parserService;
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $subcategories = Subcategory::query()->where('id', '<=', 3500);
+        $this->parserService->fillMarkas($subcategories);
+        return 0;
+    }
+}
